78eb70d797a3d80c736a80cab2bee8a1146e504d
mostafa shapan <mostafa.102556@cs.mti.edu.eg>
2024-09-18 18:12:10 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/user-attachments/assets/6a448e86-8b5b-4994-a297-85070d6e0536
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'task_home_page.dart';
+
+void main() {
+  runApp(TaskApp());
+}
+
+class TaskApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TaskHomePage(),
+    );
+  }
+}
